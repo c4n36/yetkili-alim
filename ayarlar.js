82538aaeed56021e6dbafcf50619139f157eb62b
c4n36 <alicanparlak7@gmail.com>
2024-12-03 18:12:03 +0300

    Update ayarlar.js

--- ayarlar.js
+++ ayarlar.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: '', 
-    sahipID: '',
+    token: 'MTMxMjQ5NTY1ODk4ODczMjQ1Nw.GwlFrH.r1C4JxtkQ7Vijrmq99I4lZ2B9DdVF9gfLWDmWY', 
+    sahipID: '850378342620201011',
   };
